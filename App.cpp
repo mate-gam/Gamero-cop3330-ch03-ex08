cc7fb13fe7c6dcce05d7614d17be65981c1daa11
mate-gam <gameromateo@knights.ucf.edu>
2021-11-29 10:33:11 -0500

    Initial Commit

---
+++ App.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    if (number % 2 == 0)
+    {
+        cout << "The number " << number << " is even";
+    }
+    else
+    {
+        cout << "The number " << number << " is odd";
+    }
+}
